4fc8a8c8573bdb6978bdd3aa6cd0218f30b0430b
TxemaN <txalcala@hotmail.com>
2023-07-01 13:36:43 +0200

    java explicado todo bajo el mismo addlistener

--- js/main.js
+++ js/main.js
@@ -4,46 +4,43 @@ const btnOcultar = document.querySelector("#btnOcultar")
 const bannerBajo = document.querySelector("#bannerBajo img")
 const fragment = document.createDocumentFragment();
 //con este addevent SWITCH llamamos a los botones de mostrar o no//
-document.addEventListener('click',(ev)=>{
-switch (ev.target.id) {
-    case "btnMostrar" : return pintarFotos();
-    break;
-    case "btnOcultar" : return esconderFotos();
-    break;
-    
-}})
-
-//con este addevent hacemos que todo sobre lo que se haga click con clase fotico cambia la url de la foto de abajo//
-document.addEventListener('click',(ev)=>{
+document.addEventListener('click', (ev) => {
+    switch (ev.target.id) {
+        case "btnMostrar": return pintarFotos();
+            break;
+        case "btnOcultar": return esconderFotos();
+            break;
+    }
+    //con este condicional dentro del eventlistener hacemos que todo sobre lo que se haga click con clase fotico cambia la url de la foto de abajo//
     if (ev.target.classList.contains("fotico")) {
-        
-         bannerBajo.src=ev.target.src;
-         
-         }})
+
+        bannerBajo.src = ev.target.src;
+    }
+})
 
 const arrayFotos = [{
-    id:"foto1",
+    id: "foto1",
     url: "assets/viajes-1.jpg",
     alt: "",
     titulo: "MIMIMIMIMI",
     pie: ""
 },
 {
-    id:"foto2",
+    id: "foto2",
     url: "assets/viajes-2.jpg",
     alt: "",
     titulo: "MIMIMIMIMI",
     pie: ""
 },
 {
-    id:"foto3",
+    id: "foto3",
     url: "assets/viajes-3.jpg",
     alt: "",
     titulo: "MIMIMIMIMI",
     pie: ""
 },
 {
-    id:"foto4",
+    id: "foto4",
     url: "assets/viajes-4.jpg",
     alt: "",
     titulo: "MIMIMIMIMI",
@@ -52,7 +49,7 @@ const arrayFotos = [{
 
 ]
 //Aquí creamos las fotos con los ids y las clases//
-const pintarFotos =() => {
+const pintarFotos = () => {
     arrayFotos.forEach((item) => {
         const cajaFotos = document.createElement("FIGURE");
 
@@ -80,13 +77,14 @@ const pintarFotos =() => {
 }
 
 
-const esconderFotos = () =>{
+const esconderFotos = () => {
 
     btnOcultar.classList.add("ocultar")
     btnMostrar.classList.remove("ocultar")
 
-    for(let i=0; i<arrayFotos.length; i++){
-    contenedorFotos.removeChild(contenedorFotos.children[0]);}
+    for (let i = 0; i < arrayFotos.length; i++) {
+        contenedorFotos.removeChild(contenedorFotos.children[0]);
+    }
 }
 
 
