d537419ae658cb1775679b5f74a88e564f177f1b
TxemaN <txalcala@hotmail.com>
2023-07-01 12:56:58 +0200

    java explicado

--- js/main.js
+++ js/main.js
@@ -3,23 +3,23 @@ const btnMostrar = document.querySelector("#btnMostrar")
 const btnOcultar = document.querySelector("#btnOcultar")
 const bannerBajo = document.querySelector("#bannerBajo img")
 const fragment = document.createDocumentFragment();
-
+//con este addevent SWITCH llamamos a los botones de mostrar o no//
 document.addEventListener('click',(ev)=>{
 switch (ev.target.id) {
     case "btnMostrar" : return pintarFotos();
     break;
     case "btnOcultar" : return esconderFotos();
     break;
-    case "foto1" : return bannerBajo.src=ev.target.src;
-     break;
-     case "foto2" : return bannerBajo.src=ev.target.src;
-     break;
-     case "foto3" : return bannerBajo.src=ev.target.src;
-     break;
-     case "foto4" : return bannerBajo.src=ev.target.src;
-     break;
-}}
-	)
+    
+}})
+
+//con este addevent hacemos que todo sobre lo que se haga click con clase fotico cambia la url de la foto de abajo//
+document.addEventListener('click',(ev)=>{
+    if (ev.target.classList.contains("fotico")) {
+        
+         bannerBajo.src=ev.target.src;
+         
+         }})
 
 const arrayFotos = [{
     id:"foto1",
@@ -51,7 +51,7 @@ const arrayFotos = [{
 },
 
 ]
-
+//Aquí creamos las fotos con los ids y las clases//
 const pintarFotos =() => {
     arrayFotos.forEach((item) => {
         const cajaFotos = document.createElement("FIGURE");
@@ -62,6 +62,7 @@ const pintarFotos =() => {
         const imagen = document.createElement("IMG");
         imagen.id = item.id;
         imagen.src = item.url;
+        imagen.classList.add("fotico");
 
 
         const titulillo = document.createElement("H3");
